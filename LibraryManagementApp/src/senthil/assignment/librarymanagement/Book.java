6231b7bc3fbcbfda373aef6fc4629ad151b0c5a5
Manikanth <Manikanth@LAPTOP-MTSFN3SM>
2023-10-19 15:27:16 +0530

    Commiting my librarymanagement project

---
+++ LibraryManagementApp/src/senthil/assignment/librarymanagement/Book.java
@@ -0,0 +1,13 @@
+package senthil.assignment.librarymanagement;
+
+public class Book {
+	
+    private int id;
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+    private String genre;
+
+    
+}
---
+++ LibraryManagementApp/src/senthil/assignment/librarymanagement/LibraryManagementApp.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package senthil.assignment.librarymanagement;
+
+/**
+ * 
+ */
+public class LibraryManagementApp {
+
+	/**
+	 * @param args
+	 * @author Senthil
+	 */
+	LibraryManagementApp(){
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
